5256028c515de5ead8451f8c3ae2a53f4ab86faa
vpvm96 <vpvm96@gmail.com>
2023-01-17 13:12:39 +0900

    feat. admin 권한 로직 적용

--- src/apis/firebaseService.js
+++ src/apis/firebaseService.js
@@ -6,6 +6,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth"
+import { getDatabase, ref, get } from "firebase/database"
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -17,6 +18,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const auth = getAuth()
 const provider = new GoogleAuthProvider()
+const database = getDatabase()
 
 export function firebaseGoogleLoginService() {
   signInWithPopup(auth, provider).catch(console.error())
@@ -27,7 +29,18 @@ export function firebaseLogoutService() {
 }
 
 export function firebaseUserStateChange(callback) {
-  onAuthStateChanged(auth, (user) => {
-    callback(user)
+  onAuthStateChanged(auth, async (user) => {
+    const updateUser = user ? await firebaseAdminUser(user) : null
+    callback(updateUser)
+  })
+}
+
+async function firebaseAdminUser(user) {
+  return get(ref(database, "admins")).then((snapshot) => {
+    if (snapshot.exists()) {
+      const admins = snapshot.val()
+      const isAdmin = admins.includes(user.uid)
+      return { ...user, isAdmin }
+    }
   })
 }
