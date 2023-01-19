d2a7c19b6e4d1aad5a601f262dcee30b2e91de80
vpvm96 <vpvm96@gmail.com>
2023-01-19 10:38:25 +0900

    fix. button ui style 수정

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Button = ({ text, onClick }) => {
   return (
     <button
-      className="bg-brand text-white py-2 px-4 rounder-sm hover:brightness-110"
+      className="bg-brand text-white py-2 px-4 rounder-sm hover:brightness-110 w-full"
       onClick={onClick}
     >
       {text}
