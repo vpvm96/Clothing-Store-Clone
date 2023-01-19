39c99ffea776cfc732aee31841a554984fb5b1c2
vpvm96 <vpvm96@gmail.com>
2023-01-19 10:39:25 +0900

    feat. productsDetail 페이지 스타일링 및 데이터 받음 및 옵션 선택 추가

--- src/pages/ProductsDetail.jsx
+++ src/pages/ProductsDetail.jsx
@@ -1,7 +1,50 @@
-import React from "react"
+import React, { useState } from "react"
+import { useLocation } from "react-router-dom"
+import Button from "../components/ui/Button"
 
 const ProductsDetail = () => {
-  return <div>ProductsDetail</div>
+  const {
+    state: {
+      product: { id, image, title, description, category, price, options },
+    },
+  } = useLocation()
+  const [selected, setSelected] = useState(options && options[0])
+
+  const handleSelect = (e) => setSelected(e.target.value)
+  const handleClick = (e) => {}
+
+  return (
+    <section>
+      <p className="mx-12 mt-4 text-gray-700">{category}</p>
+      <section className="flex flex-col md:flex-row p-4">
+        <img className="w-full px-4 basis-7/12" src={image} alt={title} />
+        <div className="w-full basis-5/12 flex-col p-4">
+          <h2 className="text-3xl font-bold py-2 ">{title}</h2>
+          <p className="text-2xl font-bold py-2 border-b border-gray-400">
+            {price}
+          </p>
+          <p className="py-4 text-lg">{description}</p>
+          <div className="flex items-center">
+            <label htmlFor="select" className="text-brand font-bold">
+              옵션:
+            </label>
+            <select
+              id="select"
+              className="p-2 m-4 flex-1 border-2 border-dashed border-brand outline-none"
+              onChange={handleSelect}
+              value={selected}
+            >
+              {options &&
+                options.map((option, index) => (
+                  <option key={index}>{option}</option>
+                ))}
+            </select>
+          </div>
+          <Button text="장바구니에 추가" onClick={handleClick} />
+        </div>
+      </section>
+    </section>
+  )
 }
 
 export default ProductsDetail
