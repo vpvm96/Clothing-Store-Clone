def716e5265003c14228b67b2247fe2c338c983a
vpvm96 <vpvm96@gmail.com>
2023-01-31 19:37:25 +0900

    feat. 장바구니 기능 추가

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,7 +1,56 @@
 import React from "react"
+import { useQuery } from "@tanstack/react-query"
+import { firebaseGetCart } from "../apis/firebaseService"
+import { useAuthContext } from "../context/AuthContext"
+import CartItem from "../components/CartItem"
+import PriceCard from "../components/PriceCard"
+import Icon from "../assets/icons/icon"
+import Button from "../components/ui/Button"
+
+const SHIPPING = 3000
 
 const MyCart = () => {
-  return <div>MyCart</div>
+  const { uid } = useAuthContext()
+  const { isLoading, data: products } = useQuery(["carts"], () =>
+    firebaseGetCart(uid)
+  )
+
+  if (isLoading) return <p>Loading...</p>
+
+  const hasProduct = products && products.length > 0
+  const totalPrice =
+    products &&
+    products.reduce(
+      (prev, current) => prev + parseInt(current.price) * current.quntity,
+      0
+    )
+
+  return (
+    <section className="p-8 flex-col">
+      <p className="text-2xl text-center font-bold pb-4 border-b border-gray-300">
+        내 장바구니
+      </p>
+      {!hasProduct && <p>장바구니에 상품이 없습니다.</p>}
+      {hasProduct && (
+        <>
+          <ul className="border-b border-gray-300 mb-8 p-4 px-8">
+            {products &&
+              products.map((product) => (
+                <CartItem key={product.id} product={product} uid={uid} />
+              ))}
+          </ul>
+          <div className="flex justify-between items-center mb-6 px-2 md:px-8 lg:px-16">
+            <PriceCard text="상품 총액" price={totalPrice} />
+            <Icon name="plus" />
+            <PriceCard text="배송액" price={SHIPPING} />
+            <Icon name="equals" />
+            <PriceCard text="총 가격" price={totalPrice + SHIPPING} />
+          </div>
+          <Button text="주문하기" />
+        </>
+      )}
+    </section>
+  )
 }
 
 export default MyCart
